466e068882e134d6cc00fca40bf0df6e6615ee62
kolias4 <gknikos3@gmail.com>
2019-04-19 17:36:51 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/retard.jpg';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import svino from './audio/svino.mp3';
 
 class App extends Component {
+
+  loud = (evt) => {
+    console.log(evt.target.name);
+    let sv = new Audio(svino);
+    sv.play();
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="text-center">ΡΩΤΑ ΤΟΝ ΧΡΗΣΤΟ</h1>
+
+
         </header>
+         <button name="svino" type="button" className="btn btn-primary btn-block"
+           onClick={this.loud}>
+           Πως τα βλέπεις τα πράγματα?
+         </button>
+         <button name="svino" type="button" className="btn btn-secondary btn-block"
+           onClick={this.loud}>
+           Θα ενισχύσεις οικονομικά το ΚΚΕ?
+         </button>
+         <button name="svino" type="button" className="btn btn-primary btn-block"
+           onClick={this.loud}>
+           Είδες που σου λέγαμε για τις συλλογικές συμβάσεις?
+         </button>
+         <button name="svino" type="button" className="btn btn-secondary btn-block"
+           onClick={this.loud}>
+           Πότε πιστέυεις θα καταστραφεί ο κόσμος?
+         </button>
+
       </div>
     );
   }
