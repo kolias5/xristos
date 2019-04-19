30054c81fd2a41b9f088c9aba753c1b458082a15
kolias4 <gknikos3@gmail.com>
2019-04-19 17:43:56 +0300

    Updates

---
+++ precache-manifest.ae18ad2c869f6a82b9d25cc92282d6f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e594700241ee1d4d20f6065761ed679",
+    "url": "/xristos/static/media/svino.4e594700.mp3"
+  },
+  {
+    "revision": "c88b8b68db575e003403ce463d4864d5",
+    "url": "/xristos/static/media/retard.c88b8b68.jpg"
+  },
+  {
+    "revision": "948943eb00c193cf0055",
+    "url": "/xristos/static/js/runtime~main.bdc6e7c3.js"
+  },
+  {
+    "revision": "2ee82d1b5782803e048d",
+    "url": "/xristos/static/js/main.259102e6.chunk.js"
+  },
+  {
+    "revision": "dd0a3302c57f75ec2ddb",
+    "url": "/xristos/static/js/2.00212eb8.chunk.js"
+  },
+  {
+    "revision": "2ee82d1b5782803e048d",
+    "url": "/xristos/static/css/main.2c8894e2.chunk.css"
+  },
+  {
+    "revision": "dd0a3302c57f75ec2ddb",
+    "url": "/xristos/static/css/2.3f149bd2.chunk.css"
+  },
+  {
+    "revision": "c758a4ed60e8d28aa9417d036ddae44e",
+    "url": "/xristos/index.html"
+  }
+];
